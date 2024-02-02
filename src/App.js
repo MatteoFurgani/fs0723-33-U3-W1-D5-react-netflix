a3e17e9fb2de35e95ee6846ea86454ed2a44bdc6
Matteo Furgani <151537051+MatteoFurgani@users.noreply.github.com>
2024-02-02 16:17:43 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import MyNav from "./components/MyNav";
+import GalleriaFilm from "./components/GalleriaFilm";
+import MyFooter from "./components/MyFooter";
+// import PaginaProfilo from "./components/PaginaProfilo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <MyNav />
       </header>
-    </div>
+      <main>
+        <GalleriaFilm />
+      </main>
+      <footer>
+        <MyFooter />
+      </footer>
+      {/* Qui ho inserito la pagina profilo  */}
+      {/* <PaginaProfilo /> */}
+    </>
   );
 }
 
---
+++ src/components/MyNav.jsx
@@ -0,0 +1,48 @@
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+
+const MyNavbar = () => {
+  return (
+    <Navbar expand="lg" variant="dark">
+      <Container fluid>
+        <img
+          src="../logo.png"
+          alt="Logo"
+          style={{ width: "100px", height: "55px" }}
+        />
+
+        <Navbar.Toggle aria-controls="navbarSupportedContent" />
+        <Navbar.Collapse id="navbarSupportedContent">
+          <Nav className="me-auto mb-2 mb-lg-0">
+            <Nav.Link href="#" className="fw-bold">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#" className="fw-bold">
+              TV Shows
+            </Nav.Link>
+            <Nav.Link href="#" className="fw-bold">
+              Movies
+            </Nav.Link>
+            <Nav.Link href="#" className="fw-bold">
+              Recently Added
+            </Nav.Link>
+            <Nav.Link href="#" className="fw-bold">
+              My List
+            </Nav.Link>
+          </Nav>
+          <div className="d-flex align-items-center">
+            <i className="bi bi-search icons"></i>
+            <div id="kids" className="fw-bold">
+              KIDS
+            </div>
+            <i className="bi bi-bell icons"></i>
+            <i className="bi bi-person-circle icons"></i>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MyNavbar;
